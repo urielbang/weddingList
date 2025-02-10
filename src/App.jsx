dc0a8d3943f249af41a756d044ce30c340dd7cec
urielbang <uri32655@gmail.com>
2025-02-10 12:01:09 +0200

    wedding

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+function App() {
+  const weddingTasks = [
+    { task: "קביעת תאריך", completed: false },
+    { task: "לוודא שרב מחתן בתאריך", completed: false },
+    { task: "בחירת אולם", completed: false },
+    { task: "בחירת תזמורת", completed: false },
+    { task: "בחירת צלם", completed: false },
+    { task: "קביעת פגישה עם רופאת נשים (אם רלוונטי)", completed: false },
+    { task: "רישום ברבנות (תעודת נישואין+תעודת רווקות)", completed: false },
+    { task: "הכנת רשימת מוזמנים", completed: false },
+    { task: "בחירת שמלת כלה", completed: false },
+    { task: "מציאת מדריך חתנים / מדריכת כלות", completed: false },
+    { task: "עיצוב והדפסת הזמנות", completed: false },
+    { task: "ארגון הסעות לאורחים", completed: false },
+    { task: "בחירת ברכונים או מזכרות לאורחים", completed: false },
+    { task: "בדיקות רפואיות (רופא שיניים, עיניים וכו')", completed: false },
+    { task: "רכישת נעליים לכלה ולחתן", completed: false },
+    { task: "רכישת טבעות נישואין", completed: false },
+    { task: "הכנת כתובה מיוחדת", completed: false },
+    { task: "וידוא זמינות של עדים לחתונה", completed: false },
+    { task: "תיאום קריין לחופה", completed: false },
+    { task: "ארגון ברכת כלה", completed: false },
+    { task: "תיאום מאפרת ומעצבת שיער", completed: false },
+    { task: "סידורי פרחים (כסא כלה, זר לכלה וכו')", completed: false },
+    { task: "רכישת מתנות להורים והכנת מכתב אישי", completed: false },
+    { task: "וידוא תקינות וארגון רכב הגעה לחתונה", completed: false },
+    { task: "הכנת תיק לכלה עם מגבונים, מחט, פלסטרים וכו'", completed: false },
+    { task: "וידוא שהחתן מוכן עם בגדים, טבעת וכתובה", completed: false },
+    { task: "ארגון מקומות ישיבה וקבלת פנים", completed: false },
+    {
+      task: "וידוא זמינות ומיקום של טלית, כוס לשבירה, כתובה וכו' לחופה",
+      completed: false,
+    },
+    { task: "תיאום תזמורת ולבחור שירים לחופה ולריקודים", completed: false },
+    { task: "וידוא זמינות של מערכת ההגברה", completed: false },
+    { task: "וידוא זמינות כל המברכים בחופה", completed: false },
+    { task: "וידוא זמינות ומספר הגביעים לברכה", completed: false },
+    { task: "וידוא נוכחות כל האורחים מהמשפחה הקרובה", completed: false },
+    { task: "תכנון וסידור מקומות ישיבה באולם", completed: false },
+    { task: "וידוא הכנת אוכל לזוג ללילה הראשון", completed: false },
+    { task: "תיאום לינה ללילה הראשון (בית מלון / צימר)", completed: false },
+    {
+      task: "ארגון רשימת ציוד לבית (רהיטים, כלי מטבח, מצעים וכו')",
+      completed: false,
+    },
+    { task: "תכנון התקציב הכולל של החתונה", completed: false },
+    { task: "תיאום וסגירת תשלום עם הספקים השונים", completed: false },
+    { task: "וידוא שהצילום יתבצע לפי תכנון מוקדם", completed: false },
+    { task: "ארגון מתנות לאורחים ולמשפחה", completed: false },
+    { task: "וידוא שכל הציוד הנדרש נמצא במקום האירוע", completed: false },
+  ];
+
+  return (
+    <>
+      <div className="PixelArt"></div>
+      <div className="Text">Shulmit & Uriel</div>
+    </>
+  );
+}
+
+export default App;
